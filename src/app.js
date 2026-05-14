ad08e2c15d848f3c14ca477fa441a27a02779d61
Pradeep <psaini@withpronto.com>
2026-05-14 20:53:48 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,38 +2,13 @@ const express = require("express")
 
 const app = express()
 
-
-app.get("/user", (req, res)=>{
+app.get("/user/:userId/:name/:password", (req, res)=>{
+    console.log(req.params)
     res.send({
         firstName: "Ankit",
         lastName:"Singh"
     })
-})
-
-
-app.post("/user", (req, res)=>{
-    res.send("Data saved successfully!")
-})
-
-app.delete("/user", (req, res)=>{
-    res.send("Data deleted successfully!")
-})
-
-
-app.use("/hello",(req, res) => {
-    console.log('first')
-   res.send("hello Page")
-})
-
-
-app.use("/bye",(req, res) => {
-   res.send("bye Page")
-})
-
-app.use("/",(req, res) => {
-    console.log('hello page on screem')
-   res.send("Home Page")
-})
+}) 
 
 app.listen(3000, ()=>{
     console.log('server is running')
