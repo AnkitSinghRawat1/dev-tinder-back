1d39e0892dc7c2def428e4bb2de097ae34595fca
Pradeep <psaini@withpronto.com>
2026-05-15 08:13:05 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,34 @@ const express = require("express")
 
 const app = express()
 
-app.get("/user/:userId/:name/:password", (req, res)=>{
-    console.log(req.params)
+// route hadnlers ---> (req,res)=>{}
+//  app.use("/route",rh0,[rh1,rh2], rh3 , rh4 , rh5 )
+
+app.get("/user",[ (req, res, next)=>{
+    console.log("1st Route Handler")
     res.send({
         firstName: "Ankit",
         lastName:"Singh"
     })
+    next()
+}, (req,res, next) =>{
+    console.log("2nd route handler")
+    res.send({
+        firstName: "Ankit2",
+        lastName:"Singh2"
+    }) 
+}], (req,res, next) =>{
+    console.log("3rd route handler")
+    res.send({
+        firstName: "Ankit3",
+        lastName:"Singh3"
+    }) 
+}, (req,res, next) =>{
+    console.log("4th route handler")
+    res.send({
+        firstName: "Ankit4",
+        lastName:"Singh4"
+    }) 
 }) 
 
 app.listen(3000, ()=>{
