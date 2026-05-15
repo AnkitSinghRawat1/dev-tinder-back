4b237b7761625dc562f30f4ca73be3f2083b7cdf
Pradeep <psaini@withpronto.com>
2026-05-15 22:43:49 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,22 @@
 const express = require("express");
-const {adminAuth, userAuth} = require('./middlewares/authMiddleware')
 const app = express();
 
-// route hadnlers ---> (req,res)=>{}
-//  app.use("/route",rh0,[rh1,rh2], rh3 , rh4 , rh5 )
+app.get("/getUserData", (req, res) => {
+  // try{
+    throw new Error("Something went wrong")
+    res.send("User Data Send")
+  // }catch(err){
+  //   res.status(500).send("Some error! please contact support team!")
 
-app.use("/admin", adminAuth) 
-
-app.get('/user/all',userAuth, (req, res)=>{
-    res.send("all userrs here")
+  // }
 })
 
-app.post('/user/login', (req, res) => {
-    res.send("User login successfully!")
+app.use("/", (err,req,res,next) => {
+  if(err){
+    res.status(500).send("Something went wrong")
+  
+  }
 })
-
-app.get("/admin/getAllData", (req, res, next) => {
-  res.send("Get All Data");
-});
-
-app.delete("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a User");
-});
 
 app.listen(3000, () => {
   console.log("server is running");
