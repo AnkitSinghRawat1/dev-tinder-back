b85cc712ee11f1794eabc02fc60bc70e397d8723
Pradeep <psaini@withpronto.com>
2026-06-09 12:32:25 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const { userAuth } = require("../middlewares/authMiddleware");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectRequest");
 const ConnectionRequestModel = require("../models/connectRequest");
+const User = require("../models/user");
 
 const userSafeData = "firstName lastName age photoUrl gender about skills";
 
@@ -58,6 +59,40 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       message: "User Connections !",
       data: data,
     });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1
+    let limit = parseInt(req.query.limit) || 10
+    limit = Math.min(limit, 50)
+
+
+    const skip = (page - 1) * limit
+
+    const connections = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    });
+
+    const connectIDS = connections.map((connection) => {
+      if (
+        connection.fromUserId._id.toString() === loggedInUser._id.toString()
+      ) {
+        return connection.toUserId._id.toString();
+      }
+
+      return connection.fromUserId._id.toString();
+    });
+
+    const users = await User.find({ _id: { $nin: Array.from([...connectIDS, loggedInUser._id.toString()]) } })
+    .select(userSafeData).skip(skip).limit(limit)
+
+    res.status(200).json({ message: "Users found", data: users });
   } catch (err) {
     res.status(400).send(err.message);
   }
